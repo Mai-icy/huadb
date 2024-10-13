9c566caef3ffd42927171c99d76524e9352b3e64
Maiicy <Maiicy@foxmail.com>
2024-10-13 14:47:12 +0800

    Complete the LRU

--- src/storage/lru_buffer_strategy.cpp
+++ src/storage/lru_buffer_strategy.cpp
@@ -4,14 +4,22 @@ namespace huadb {
 
 void LRUBufferStrategy::Access(size_t frame_no) {
   // 缓存页面访问
-  // LAB 1 BEGIN
-  return;
+  // LAB 1 DONE
+  auto it = cache_map_.find(frame_no);
+  if(it == cache_map_.end()){
+    cache_items_.insert(cache_items_.begin(), frame_no);
+    cache_map_[frame_no] = cache_items_.begin();
+  }else{
+    cache_items_.splice(cache_items_.begin(), cache_items_, it->second);
+  }
 };
 
 size_t LRUBufferStrategy::Evict() {
   // 缓存页面淘汰，返回淘汰的页面在 buffer pool 中的下标
   // LAB 1 BEGIN
-  return 0;
+  size_t res = cache_items_.back();
+  cache_items_.pop_back();
+  return res;
 }
 
 }  // namespace huadb
--- src/storage/lru_buffer_strategy.h
+++ src/storage/lru_buffer_strategy.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "storage/buffer_strategy.h"
+#include <unordered_map>
+#include <list>
 
 namespace huadb {
 
@@ -8,6 +10,9 @@ class LRUBufferStrategy : public BufferStrategy {
  public:
   void Access(size_t frame_no) override;
   size_t Evict() override;
+ private:
+  std::list<size_t> cache_items_;
+  std::unordered_map<size_t, std::list<size_t>::iterator> cache_map_;
 };
 
 }  // namespace huadb
